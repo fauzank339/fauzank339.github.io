34f81aaefba4f9c01001df266ff897d5758779fb
Abdullah Kidwai <50906187+fauzank339@users.noreply.github.com>
2022-12-23 12:51:33 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+//lerp = linear interpolation
+function lerp(a,b,t){
+    return a+(b-a)*t;
+}
+
+function vLerp(A,B,t){
+    const res={};
+    for(let attr in A){
+        res[attr]=lerp(A[attr],B[attr],t);
+    }
+     
+    return res;
+}
---
+++ script.js
@@ -0,0 +1,87 @@
+myCanvas.width = 500;
+myCanvas.height = 300;
+
+
+const n=20; //sorting 20 elements will store them in an array
+const array=[];
+const stringHeight = myCanvas.height*0.45;
+
+const socks=[]; //these are columns represented by socks
+
+
+const margin = 30;
+const availableWidth=myCanvas.width-margin*2;
+const spacing=availableWidth/n;
+
+for(let i=0;i<n;i++){
+    array[i]=Math.random();
+}
+
+
+
+
+
+
+for(let i=0;i<array.length;i++)
+{
+    const x = i*spacing+spacing/2+margin;
+    const y = stringHeight;
+    const height = myCanvas.height*0.4*array[i];
+    socks[i]=new Sock(x,y,height);
+    
+}
+
+const moves=bubbleSort(array);
+
+const ctx = myCanvas.getContext("2d");
+animate();
+
+function animate(){
+    ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
+
+    ctx.beginPath();
+    ctx.moveTo(0,stringHeight);
+    ctx.lineTo(myCanvas.width,stringHeight);
+    ctx.stroke();
+
+    let changed = false;
+    for (let i=0;i<socks.length;i++)
+    {
+        changed=socks[i].draw(ctx)||changed;
+    }
+
+    if(!changed && moves.length>0){
+        const nextMove=moves.shift();
+        if(nextMove.type=="swap"){
+            const [i,j]=nextMove.indices;
+            socks[i].moveTo(socks[j].loc);
+            socks[j].moveTo(socks[i].loc);
+            [socks[i],socks[j]]=[socks[j],socks[i]];
+        }
+    }
+
+    requestAnimationFrame(animate);
+}
+
+function bubbleSort(array){
+    const moves=[];
+    do{
+        var swapped=false;
+        for (let i=1;i<array.length;i++){
+            moves.push({
+                indices:[i-1,i],
+                type:"comparison"
+            });
+            if(array[i-1]>array[i]){
+                swapped=true;
+                [array[i-1],array[i]]=[array[i],array[i-1]];
+                moves.push({
+                    indices:[i-1,i],
+                    type:"swap"
+                });
+            }
+        }
+
+    }while(swapped);
+    return moves;
+}
